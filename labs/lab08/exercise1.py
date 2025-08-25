a7330a638af02e643841dbd66282b634af74fc86
syahdiq <syahdiqasyraff@gmail.com>
2025-08-25 13:07:44 +0800

    cakkkkk

---
+++ labs/lab08/exercise1.py
@@ -0,0 +1,14 @@
+score1 = 85
+score2 = 92.5
+score3 = 78
+average = (score1 + score2 + score3)/3
+
+print(average)
+print(int(average))
+
+random = score1 ** 2 / score2 + score3 % 7
+print(random)
+
+
+print(int(score2) , "vs" , float(score1))
+
